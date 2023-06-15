72450a7fc679441b6b13c6ce5ef63192dcdd9813
Maas Lalani <maas@lalani.dev>
2023-06-15 15:17:22 -0400

    fix: allow --help with API key

--- main.go
+++ main.go
@@ -27,6 +27,12 @@ var rootCmd = &cobra.Command{
 	Short: "email is a command line interface for sending emails.",
 	Long:  `email is a command line interface for sending emails.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		if os.Getenv(RESEND_API_KEY) == "" {
+			fmt.Printf("\n  %s %s %s\n\n", errorHeaderStyle.String(), inlineCodeStyle.Render(RESEND_API_KEY), "environment variable is required.")
+			fmt.Printf("  %s %s\n\n", commentStyle.Render("You can grab one at"), linkStyle.Render("https://resend.com"))
+			os.Exit(1)
+		}
+
 		if hasStdin() {
 			b, err := io.ReadAll(os.Stdin)
 			if err != nil {
@@ -82,13 +88,6 @@ func init() {
 }
 
 func main() {
-	key := os.Getenv(RESEND_API_KEY)
-	if key == "" {
-		fmt.Printf("\n  %s %s %s\n\n", errorHeaderStyle.String(), inlineCodeStyle.Render(RESEND_API_KEY), "environment variable is required.")
-		fmt.Printf("  %s %s\n\n", commentStyle.Render("You can grab one at"), linkStyle.Render("https://resend.com"))
-		os.Exit(1)
-	}
-
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
